d4aad0881b9bbe270445731177b0fef599d4d9e8
Filip-Konkowski <fkonkowski@gmail.com>
2015-10-09 11:26:53 +0200

    Initial commit

---
+++ book/index.php
@@ -0,0 +1,44 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!--<link rel="stylesheet" href="css/custom.css"> -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- icon font awesome CSS -->
+    <link rel="stylesheet" href="css/extra/font_awesome.css">
+    <!-- normalize CSS -->
+    <link rel="stylesheet" href="css/extra/normalize.min.css">
+    <!-- bootstrap CSS -->
+    <link rel="stylesheet" media="screen" href="css/css_bootstrap/bootstrap.min.css">
+    <title>Books </title>
+</head>
+<body>
+<div class="jumbotron">
+    <h1>Wypożyczalnia książek</h1>
+    <p>Witaj w mojej wypożyczalni książek</p>
+</div>
+jkhykjhkjh
+<!-- div okalajacy cala szerokosc strony-->
+    <div class="container">
+        <div class="row">
+           <div class="col-md-12 col-lg-12">
+
+           </div>
+        </div>
+
+    </div>
+
+
+
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/js_bootstrap/bootstrap.min.js/"></script>
+<!--<script src="js/custom.js"></script> -->
+</body>
+</html>
+
